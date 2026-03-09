cbd2a9ecfb1eb91a656c5b393e8061a33188f065
replimok <replimok@gmail.com>
2026-03-09 18:54:07 +0300

    remove test.db

--- tests/test_shorter.py
+++ tests/test_shorter.py
@@ -37,6 +37,9 @@ def client():
     with TestClient(app) as test_client:
         yield test_client
     Base.metadata.drop_all(bind=engine)
+    #
+    if os.path.exists("test.db"):
+        os.remove("test.db")
 
 
 def test_create_short_id(client):
